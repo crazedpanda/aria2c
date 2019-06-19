a0ccdc2c85b4f00ebba7a4d232fa930030b5634b
chouuohc87 <chouuohc87@yahoo.com>
2019-06-19 13:42:03 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var fs = require('fs');
+var path = require('path');
 var app = express();
 var WebTorrent = require('webtorrent');
 var client = new WebTorrent();
@@ -142,7 +144,7 @@ app.get('/stream/:infoHash', function(req, res) {
 });
 ///////////////////////////////
 app.get('/files/:infoHash', function(req, res) {
-    res.status(200).send(JSON.stringify(getFilePaths('/tmp/webtorrent')));
+    res.status(200).send(JSON.stringify(getFilePaths('/tmp/webtorrent' + req.params.infoHash)));
 });
 
 function getFilePaths(dir) {
