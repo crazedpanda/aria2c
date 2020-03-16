072e9e113e447789142fa2cb92ae6a51e887c953
chouuohc87 <chouuohc87@yahoo.com>
2020-03-15 20:21:49 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
 var express = require('express');
-var fs = require('fs');
-var path = require('path');
-var cookieParser = require('cookie-parser')
-var app = express();
 var WebTorrent = require('webtorrent-hybrid');
 var client = new WebTorrent();
 var port = process.env.PORT ? process.env.PORT : 3000;
@@ -10,8 +6,7 @@ var buildMagnetURI = function(infoHash) {
 	return 'magnet:?xt=urn:btih:' + infoHash + '&tr=http%3A%2F%2F9.rarbg.com%3A2710%2Fannounce&tr=http%3A%2F%2Fannounce.opensharing.org%3A2710%2Fannounce&tr=http%3A%2F%2Fannounce.torrentsmd.com%3A6969%2Fannounce&tr=http%3A%2F%2Fbt.careland.com.cn%3A6969%2Fannounce&tr=http%3A%2F%2Fbttrack.9you.com%2Fannounce&tr=http%3A%2F%2Fi.bandito.org%2Fannounce&tr=http%3A%2F%2Fmgtracker.org%3A2710%2Fannounce&tr=http%3A%2F%2Fopen.acgtracker.com%3A1096%2Fannounce&tr=http%3A%2F%2Fopen.demonii.com%3A1337%2Fannounce&tr=http%3A%2F%2Fopen.demonii.com%3A6969%2Fannounce&tr=http%3A%2F%2Fopen.nyaatorrents.info%3A6544%2Fannounce&tr=http%3A%2F%2Fopensharing.org%3A2710%2Fannounce&tr=http%3A%2F%2Fpubt.net%3A2710%2Fannounce&tr=http%3A%2F%2Fretracker.telecom.kz%3A80%2Fannounce&tr=http%3A%2F%2Ftracker4.infohash.org%3A80%2Fannounce&tr=http%3A%2F%2Ftracker4.infohash.org%3A6969%2Fannounce&tr=http%3A%2F%2Ftracker.ccc.de%3A80%2Fannounce&tr=http%3A%2F%2Ftracker.dutchtracking.com%3A80%2Fannounce&tr=http%3A%2F%2Ftracker.dutchtracking.nl%3A80%2Fannounce&tr=http%3A%2F%2Ftracker.ex.ua%3A80%2Fannounce&tr=http%3A%2F%2Ftracker.frostwire.com%3A6969%2Fannounce&tr=http%3A%2F%2Ftracker.kicks-ass.net%3A80%2Fannounce&tr=http%3A%2F%2Ftracker.leechers-paradise.org%3A80%2Fannounce&tr=http%3A%2F%2Ftracker.leechers-paradise.org%3A6969%2Fannounce&tr=http%3A%2F%2Ftracker.mininova.org%2Fannounce&tr=http%3A%2F%2Ftracker.openbittorrent.com%3A80%2Fannounce&tr=http%3A%2F%2Ftracker.openbittorrent.com%3A6969%2Fannounce&tr=http%3A%2F%2Ftracker.publicbt.com%3A80%2Fannounce&tr=http%3A%2F%2Ftracker.publicbt.com%3A6969%2Fannounce&tr=http%3A%2F%2Ftracker.pubt.net%3A2710%2Fannounce&tr=http%3A%2F%2Ftracker.tfile.me%3A80%2Fannounce&tr=http%3A%2F%2Ftracker.x4w.co%3A6969%2Fannounce&tr=udp%3A%2F%2F9.rarbg.com%3A80%2Fannounce&tr=udp%3A%2F%2F9.rarbg.com%3A2710%2Fannounce&tr=udp%3A%2F%2F9.rarbg.me%3A2710%2Fannounce&tr=udp%3A%2F%2Fannounce.opensharing.org%3A2710%2Fannounce&tr=udp%3A%2F%2Fbt.careland.com.cn%3A6969%2Fannounce&tr=udp%3A%2F%2Fcoppersurfer.tk%3A6969%2Fannounce&tr=udp%3A%2F%2Fexodus.desync.com%3A80%2Fannounce&tr=udp%3A%2F%2Fexodus.desync.com%3A6969%2Fannounce&tr=udp%3A%2F%2Fmgtracker.org%3A2710%2Fannounce&tr=udp%3A%2F%2Fopen.acgtracker.com%3A1096%2Fannounce&tr=udp%3A%2F%2Fopen.demonii.com%3A1337&tr=udp%3A%2F%2Fopen.demonii.com%3A1337%2Fannounce&tr=udp%3A%2F%2Fopen.demonii.com%3A6969%2Fannounce&tr=udp%3A%2F%2Fopen.nyaatorrents.info%3A6544%2Fannounce&tr=udp%3A%2F%2Fopensharing.org%3A2710%2Fannounce&tr=udp%3A%2F%2Fpubt.net%3A2710%2Fannounce&tr=udp%3A%2F%2Fretracker.telecom.kz%3A80%2Fannounce&tr=udp%3A%2F%2Ftracker4.infohash.org%3A80%2Fannounce&tr=udp%3A%2F%2Ftracker4.infohash.org%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.blackunicorn.xyz%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.btzoo.eu%3A80%2Fannounce&tr=udp%3A%2F%2Ftracker.ccc.de%3A80&tr=udp%3A%2F%2Ftracker.ccc.de%3A80%2Fannounce&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%2Fannounce&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.dutchtracking.com%3A80%2Fannounce&tr=udp%3A%2F%2Ftracker.dutchtracking.nl%3A80%2Fannounce&tr=udp%3A%2F%2Ftracker.ex.ua%3A80%2Fannounce&tr=udp%3A%2F%2Ftracker.ilibr.org%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.istole.it%3A80&tr=udp%3A%2F%2Ftracker.kicks-ass.net%3A80%2Fannounce&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A80%2Fannounce&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80%2Fannounce&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.publicbt.com%3A80&tr=udp%3A%2F%2Ftracker.publicbt.com%3A80%2Fannounce&tr=udp%3A%2F%2Ftracker.publicbt.com%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.pubt.net%3A2710%2Fannounce&tr=udp%3A%2F%2Ftracker.x4w.co%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.zer0day.to%3A1337%2Fannounce';
 };
 var getFile = function(torrent, fileIndex) {
-	var file = torrent.files[fileIndex];
-	return file;
+	return torrent.files[fileIndex];
 };
 var getLargestFile = function(torrent) {
 	var file;
@@ -22,188 +17,123 @@ var getLargestFile = function(torrent) {
 	}
 	return file;
 };
-app.use(cookieParser());
+var app = express();
 app.use(express.static(__dirname + '/public'));
 app.use('/download', express.static('/tmp/webtorrent'));
-///////////////////////////////
 app.get('/', function(req, res) {
 	res.send('<title>MiPeerFlix</title>Hello World!');
 });
-///////////////////////////////
 app.get('/clear', function(req, res) {
-	try {
-		client.torrents.forEach(function(value, key) {
-			console.log('Removed:', value.infoHash);
-			client.remove(value.infoHash);
-		});
-		res.send('<title>MiPeerFlix - Clear</title>Removed all!');
-	} catch (err) {
-		res.send('<title>MiPeerFlix - Error</title>' + err.toString());
-	}
+	client.destroy(function(err) {
+		if (err) {
+			res.send('<title>MiPeerFlix - Error</title>' + err.toString());
+		} else {
+			res.send('<title>MiPeerFlix - Clear</title>Removed all!');
+		}
+	});
 });
-///////////////////////////////
 app.get('/list', function(req, res) {
-	try {
-		var html = '<title>MiPeerFlix - List</title>';
-		client.torrents.forEach(function(value, key) {
-			html += '<a href="/' + value.infoHash + '">' + value.infoHash + '</a><br>';
-		});
-		res.send(html);
-	} catch (err) {
-		res.send('<title>MiPeerFlix - Error</title>' + err.toString());
-	}
+	var html = '<title>MiPeerFlix - List</title>';
+	client.torrents.forEach(function(value, key) {
+		html += '<a href="/' + value.infoHash + '">' + value.infoHash + '</a><br>';
+	});
+	res.send(html);
 });
-///////////////////////////////
 app.get('/:infoHash', function(req, res) {
-	try {
-		var torrent = client.get(req.params.infoHash);
-		if (torrent) {
-			if (torrent.files.length) {
-				var html = '<title>MiPeerFlix - ' + req.params.infoHash.toLowerCase() + '</title><b>Torrent Menu:</b> <a href="/remove/' + req.params.infoHash + '">Remove</a> | <a href="/' + req.params.infoHash + '">Reload</a><br><b>Number of Peers:</b> ' + torrent.numPeers + '<hr>';
-				torrent.files.forEach(function(file, key) {
-					html += '<table class="torrent" id="' + req.params.infoHash.toLowerCase() + '" style="table-layout:fixed;width:100%"><tr class="filepath"><td style="font-weight:bold;width:140px;vertical-align:middle">File Path:</td><td>' + file.path + '</td></tr><tr class="filesize"><td style="font-weight:bold;width:140px;vertical-align:middle">File Size:</td><td>' + file.length + ' bytes</td></tr><tr class="fileprogress"><td style="font-weight:bold;width:140px;vertical-align:middle">Download Progress:</td><td>' + Math.floor(file.progress * 100) + '%</td></tr><tr class="buttons"><td></td><td><a href="/stream/' + req.params.infoHash.toLowerCase() + '/' + key + '">Stream</a>';
-					if (file.progress == 1) {
-						html += ' | <a href="/download/' + req.params.infoHash.toLowerCase() + '/' + file.path + '">Download</a>';
-					}
-					html += '</td></tr></table>';
-					if (torrent.files.length - 1 != key) {
-						html += '<hr>';
-					}
-				});
-				if (torrent.progress < 1) {
-					html += '<script>window.onload = function() { setTimeout("location.reload(true);", 15000); };</script>';
-				}
-				res.send(html);
-			} else {
-				if ('redirects' in req.cookies) {
-					if (parseInt(req.cookies.redirects) < 4) {
-						res.cookie('redirects', parseInt(req.cookies.redirects) + 1);
-					} else {
-						res.clearCookie('redirects');
-						if (client.get(req.params.infoHash)) {
-							console.log('Removed:', req.params.infoHash);
-							client.remove(req.params.infoHash);
-						}
-						throw new Error('No peers for ' + req.params.infoHash + '!');
-					}
-				} else {
-					res.cookie('redirects', 1);
-				}
-				res.redirect('/' + req.params.infoHash);
+	var torrent = client.get(req.params.infoHash);
+	if (torrent) {
+		torrent.on('noPeers', function(announceType) {
+			res.send('No peers for ' + req.params.infoHash + '!');
+		});
+		if (torrent.files.length) {
+			var html = '<head>';
+			if (torrent.progress < 1) {
+				html += '<meta http-equiv="refresh" content="15"/>';
 			}
-		} else {
-			var magnetURI = buildMagnetURI(req.params.infoHash);
-			client.add(magnetURI, function(torrent) {
-				console.log('Added:', req.params.infoHash);
-				res.redirect('/' + req.params.infoHash);
+			html += '<title>MiPeerFlix - ' + req.params.infoHash.toLowerCase() + '</title><b>Torrent Menu:</b> <a href="/remove/' + req.params.infoHash + '">Remove</a> | <a href="/' + req.params.infoHash + '">Reload</a><br><b>Number of Peers:</b> ' + torrent.numPeers + '<hr>';
+			torrent.files.forEach(function(file, key) {
+				html += '<table class="torrent" id="' + req.params.infoHash.toLowerCase() + '" style="table-layout:fixed;width:100%"><tr class="filepath"><td style="font-weight:bold;width:140px;vertical-align:middle">File Path:</td><td>' + file.path + '</td></tr><tr class="filesize"><td style="font-weight:bold;width:140px;vertical-align:middle">File Size:</td><td>' + file.length + ' bytes</td></tr><tr class="fileprogress"><td style="font-weight:bold;width:140px;vertical-align:middle">Download Progress:</td><td>' + Math.floor(file.progress * 100) + '%</td></tr><tr class="buttons"><td></td><td><a href="/stream/' + req.params.infoHash.toLowerCase() + '/' + key + '">Stream</a>';
+				if (file.progress == 1) {
+					html += ' | <a href="/download/' + req.params.infoHash.toLowerCase() + '/' + file.path + '">Download</a>';
+				}
+				html += '</td></tr></table>';
+				if (torrent.files.length - 1 != key) {
+					html += '<hr>';
+				}
 			});
+			res.send(html);
 		}
-	} catch (err) {
-		res.send('<title>MiPeerFlix - Error</title>' + err.toString());
+		res.redirect('/' + req.params.infoHash);
+	} else {
+		var magnetURI = buildMagnetURI(req.params.infoHash);
+		client.add(magnetURI, function(torrent) {
+			console.log('Added:', req.params.infoHash);
+			res.redirect('/' + req.params.infoHash);
+		});
 	}
 });
-///////////////////////////////
 app.get('/remove/:infoHash', function(req, res) {
-	try {
-		if (client.get(req.params.infoHash)) {
-			console.log('Removed:', req.params.infoHash);
-			client.remove(req.params.infoHash);
-			res.send('<title>MiPeerFlix - Remove</title>Removed: ' + req.params.infoHash);
-		} else {
-			res.send('<title>MiPeerFlix - Remove</title>Torrent does not exist!');
-		}
-	} catch (err) {
-		res.send('<title>MiPeerFlix - Error</title>' + err.toString());
+	if (client.get(req.params.infoHash)) {
+		console.log('Removed:', req.params.infoHash);
+		client.remove(req.params.infoHash);
+		res.send('<title>MiPeerFlix - Remove</title>Removed: ' + req.params.infoHash);
+	} else {
+		res.send('<title>MiPeerFlix - Remove</title>Torrent does not exist!');
 	}
 });
-///////////////////////////////
-app.get('/stream/:infoHash', function(req, res) {
-	try {
-		var torrent = client.get(req.params.infoHash);
-		if (torrent) {
-			var file = getLargestFile(torrent);
-			if (file) {
-				var range = req.headers.range;
-				if (range) {
-					var parts = range.replace(/bytes=/, "").split("-");
-					var start = parseInt(parts[0], 10);
-					var end = parts[1] ? parseInt(parts[1], 10) : file.length - 1;
-					var chunksize = (end - start) + 1;
-					var stream = file.createReadStream({
-						start: start,
-						end: end
-					});
-					var head = {
-						'Content-Range': `bytes ${start}-${end}/${file.length}`,
-						'Accept-Ranges': 'bytes',
-						'Content-Length': chunksize,
-						'Content-Type': 'video/mp4',
-					};
-					res.writeHead(206, head);
-					stream.pipe(res);
-				} else {
-					file.createReadStream({
-						start: 0,
-						end: file.length
-					}).pipe(res);
-				}
-			} else {
-				res.redirect('/stream/' + req.params.infoHash);
-			}
-		} else {
-			var magnetURI = buildMagnetURI(req.params.infoHash);
-			client.add(magnetURI, function(torrent) {
-				console.log('Added:', req.params.infoHash);
-				res.redirect('/stream/' + req.params.infoHash);
-			});
-		}
-	} catch (err) {
-		res.send('<title>MiPeerFlix - Error</title>' + err.toString());
-	}
-});
-///////////////////////////////
-app.get('/stream/:infoHash/:fileIndex', function(req, res) {
-	try {
-		var torrent = client.get(req.params.infoHash);
-		if (torrent) {
+app.get('/stream/:infoHash/:fileIndex?', function(req, res) {
+	var torrent = client.get(req.params.infoHash);
+	if (torrent) {
+		torrent.on('noPeers', function(announceType) {
+			res.send('No peers for ' + req.params.infoHash + '!');
+		});
+		if ('fileIndex' in req.params) {
 			var file = getFile(torrent, req.params.fileIndex);
-			if (file) {
-				var range = req.headers.range;
-				if (range) {
-					var parts = range.replace(/bytes=/, "").split("-");
-					var start = parseInt(parts[0], 10);
-					var end = parts[1] ? parseInt(parts[1], 10) : file.length - 1;
-					var chunksize = (end - start) + 1;
-					var stream = file.createReadStream({
-						start: start,
-						end: end
-					});
-					var head = {
-						'Content-Range': `bytes ${start}-${end}/${file.length}`,
-						'Accept-Ranges': 'bytes',
-						'Content-Length': chunksize,
-						'Content-Type': 'video/mp4',
-					};
-					res.writeHead(206, head);
-					stream.pipe(res);
-				} else {
-					file.createReadStream({
-						start: 0,
-						end: file.length
-					}).pipe(res);
-				}
+		} else {
+			var file = getLargestFile(torrent);
+		}
+		if (file) {
+			var range = req.headers.range;
+			if (range) {
+				var parts = range.replace(/bytes=/, "").split("-");
+				var start = parseInt(parts[0], 10);
+				var end = parts[1] ? parseInt(parts[1], 10) : file.length - 1;
+				var chunksize = end - start + 1;
+				var stream = file.createReadStream({
+					start: start,
+					end: end
+				});
+				var head = {
+					'Content-Range': `bytes ${start}-${end}/${file.length}`,
+					'Accept-Ranges': 'bytes',
+					'Content-Length': chunksize,
+					'Content-Type': 'video/mp4',
+				};
+				res.writeHead(206, head);
+				stream.pipe(res);
 			} else {
-				res.redirect('/stream/' + req.params.infoHash + '/' + req.params.fileIndex);
+				file.createReadStream({
+					start: 0,
+					end: file.length
+				}).pipe(res);
 			}
 		} else {
-			var magnetURI = buildMagnetURI(req.params.infoHash);
-			client.add(magnetURI, function(torrent) {
-				console.log('Added:', req.params.infoHash);
-				res.redirect('/stream/' + req.params.infoHash + '/' + req.params.fileIndex);
-			});
+			var redirectURL = '/stream/' + req.params.infoHash;
+			if ('fileIndex' in req.params) {
+				redirectURL += '/' + req.params.fileIndex;
+			}
+			res.redirect(redirectURL);
 		}
-	} catch (err) {
-		res.send('<title>MiPeerFlix - Error</title>' + err.toString());
+	} else {
+		var magnetURI = buildMagnetURI(req.params.infoHash);
+		client.add(magnetURI, function(torrent) {
+			console.log('Added:', req.params.infoHash);
+			var redirectURL = '/stream/' + req.params.infoHash;
+			if ('fileIndex' in req.params) {
+				redirectURL += '/' + req.params.fileIndex;
+			}
+			res.redirect(redirectURL);
+		});
 	}
 });
 app.listen(port, function() {
