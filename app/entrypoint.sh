70c386961a38356592aac1a265614b5b7dd343af
chouuohc87 <chouuohc87@yahoo.com>
2020-07-21 23:27:10 +0000

    0

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -4,4 +4,6 @@ cd /app/nodejs
 npm config set proxy http://138.68.240.218:8080
 npm config set https-proxy http://138.68.240.218:8080
 npm install
+npm config rm proxy
+npm config rm https-proxy
 npm start
