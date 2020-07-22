9a342807c0882dfd633d39419722be803ba16641
chouuohc87 <chouuohc87@yahoo.com>
2020-07-22 00:06:32 +0000

    0

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 set -ex
 cd /app/nodejs
-npm install https://github.com/jscissr/http-node
+npm install http://github.com/jscissr/http-node
 npm install bluebird
 npm install body-parser
 npm install compression
