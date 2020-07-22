871233305265762367d2f345597d2bfc886df091
chouuohc87 <chouuohc87@yahoo.com>
2020-07-22 01:18:22 +0000

    0

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -1,11 +1,4 @@
 #! /bin/sh
 set -ex
 cd /app/nodejs
-npm install bluebird
-npm install body-parser
-npm install compression
-npm install express
-npm install file-type
-npm install fs-extra
-npm install webtorrent
 npm start
