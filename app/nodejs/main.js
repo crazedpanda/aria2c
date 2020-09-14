644979685e4aeeff7c9ce24a23831223f7a451de
chouuohc87 <chouuohc87@yahoo.com>
2020-09-14 09:34:24 +0000

    0

--- app/nodejs/main.js
+++ app/nodejs/main.js
@@ -185,6 +185,7 @@ app.listen(process.env.PORT || 3000);
 
 function streamFile(req, res, result) {
 	var file = result.file;
+    console.log("file", file);
 	var contenttype = result.contenttype;
 	var range = req.headers.range;
 	if (range) {
@@ -212,7 +213,8 @@ function streamFile(req, res, result) {
 }
 
 function serveFile(req, res, result) {
-	var file = result.file;
+    var file = result.file;
+    console.log("file", file);
 	var contenttype = result.contenttype;
 	var head = {
 		"Content-Disposition": "filename=" + file.name,
