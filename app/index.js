26f5753d642e1445f7f6e533552dac53212bfdbf
chouuohc87 <chouuohc87@yahoo.com>
2021-05-08 18:01:19 -0500

    Update index.js

--- app/index.js
+++ app/index.js
@@ -263,8 +263,8 @@ function convertFile(req, res, file) {
 		} else {
 			exec("ffprobe -v error -select_streams v:0 -show_entries stream=height -of default=noprint_wrappers=1:nokey=1 \"/tmp/webtorrent/" + req.params.infoHash + "/" + file.path + "\"").then(function(height) {
 				if (parseInt(height.trim()) > 2160) {
-					ffmpeg(file.createReadStream()).outputOptions("-threads", parseInt(Math.floor(os.cpus().length * 0.125)), "-c:v", "libx264", "-profile:v", "baseline", "-vf", "scale=-2:720:flags=lanczos", "-c:a", "copy", "-movflags", "+faststart", "-tune", "zerolatency", "-start_number", 0, "-hls_time", 5, "-hls_list_size", 0, "-f", "hls").output("/tmp/webtorrent/" + req.params.infoHash + "/" + file.path + ".m3u8").on("progress", function(progress) {
-						console.log("Processing: " + progress.percent + "% done!");
+					ffmpeg(file.createReadStream()).outputOptions("-threads", parseInt(Math.floor(os.cpus().length * 0.125)), "-max_muxing_queue_size", 1024, "-c:v", "libx264", "-profile:v", "baseline", "-vf", "scale=-2:720:flags=lanczos", "-c:a", "copy", "-movflags", "+faststart", "-tune", "zerolatency", "-start_number", 0, "-hls_time", 10, "-hls_list_size", 0, "-f", "hls").output("/tmp/webtorrent/" + req.params.infoHash + "/" + file.path + ".m3u8").on("progress", function(progress) {
+						console.log("Processing: ", progress);
 					}).on("error", function(err) {
 						console.log("An error occurred: " + err);
 					}).on("end", function() {
