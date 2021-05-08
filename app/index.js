5f6a368ed63108b803a77682936a53d02af07c6a
chouuohc87 <chouuohc87@yahoo.com>
2021-05-07 20:16:24 -0500

    Update index.js

--- app/index.js
+++ app/index.js
@@ -229,7 +229,12 @@ const gritty = require("gritty");
 gritty.listen(io);
 
 function convertFile(req, res, file) {
-	exec("ffmpeg -i \"/tmp/webtorrent/" + req.params.infoHash + "/" + file.path + "\" -c copy -start_number 0 -hls_time 10 -hls_list_size 0 -f hls \"/tmp/webtorrent/" + req.params.infoHash + "/" + file.path + ".m3u8\" && touch \"/tmp/webtorrent/" + req.params.infoHash + "/" + file.path + ".done\"");
+	var vcodec = await exec("ffprobe -v error -select_streams v:0 -show_entries stream=codec_name -of default=noprint_wrappers=1:nokey=1 \"/tmp/webtorrent/" + req.params.infoHash + "/" + file.path + "\"");
+	if (vcodec.trim() == "h264") {
+		exec("ffmpeg -i \"/tmp/webtorrent/" + req.params.infoHash + "/" + file.path + "\" -c copy -start_number 0 -hls_time 10 -hls_list_size 0 -f hls \"/tmp/webtorrent/" + req.params.infoHash + "/" + file.path + ".m3u8\" && touch \"/tmp/webtorrent/" + req.params.infoHash + "/" + file.path + ".done\"");
+	} else {
+		exec("ffmpeg -i \"/tmp/webtorrent/" + req.params.infoHash + "/" + file.path + "\" -c:v libx264 -profile:v high -level 4.2 -movflags +faststart -c:a copy -start_number 0 -hls_time 10 -hls_list_size 0 -f hls \"/tmp/webtorrent/" + req.params.infoHash + "/" + file.path + ".m3u8\" && touch \"/tmp/webtorrent/" + req.params.infoHash + "/" + file.path + ".done\"");
+	}
 	if (req.params.index) {
 		res.redirect("/check/" + req.params.infoHash + "/" + req.params.index);
 	} else {
