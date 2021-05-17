9a3465056d9f4dc6ce7808354ab87a9ab0487c79
chouuohc87 <chouuohc87@yahoo.com>
2021-05-16 21:16:59 -0500

    Update webtorrent.js

--- app/routes/webtorrent.js
+++ app/routes/webtorrent.js
@@ -6,88 +6,31 @@ const crypto = require("crypto");
 const FileType = require("file-type");
 const fs = require("fs-extra");
 const parseRange = require("range-parser");
+const promisify = require("util").promisify
+const sleep = promisify(setTimeout);
 const WebTorrent = require("webtorrent");
 const WebSocket = require("ws");
-const clients = {};
+var client = new WebTorrent();
 router.ws("/:infoHash", function(ws, req) {
 	const infoHash = req.params.infoHash.toLowerCase();
-	if (!(infoHash in clients)) {
-		clients[infoHash] = new WebTorrent();
-	}
-	var torrent = clients[infoHash].get(infoHash);
+	var torrent = client.get(infoHash);
 	if (!torrent) {
-		torrent = clients[infoHash].add(buildMagnetURI(infoHash));
+		torrent = client.add(buildMagnetURI(infoHash));
 	}
 	updateStatus(ws, torrent);
-	ws.on("close", function() {
-		clients[infoHash].destroy();
-		delete clients[infoHash];
-	});
-});
-router.ws("/convert/:infoHash/:index?", async function(ws, req) {
-	const infoHash = req.params.infoHash.toLowerCase();
-	if (infoHash.length == 40) {
-		if (!(infoHash in clients)) {
-			clients[infoHash] = new WebTorrent();
-		}
-		var torrent = clients[infoHash].get(infoHash);
-		if (!torrent) {
-			torrent = clients[infoHash].add(buildMagnetURI(infoHash));
-		}
-		if (torrent.ready) {
-			var index;
-			if (req.params.index) {
-				index = parseInt(req.params.index) - 1;
-			} else {
-				index = torrent.files.reduce(function(total, currentValue, currentIndex, arr) {
-					return currentValue.length > arr[total].length ? currentIndex : total;
-				}, 0);
-			}
-			if (torrent.files[index] && torrent.files[index].progress == 1) {
-				const controller = new AbortController();
-				const {
-					signal
-				} = controller;
-				ws.on("close", function() {
-					controller.abort();
-				});
-				await convertVideo(signal, "https://mipeerflix.herokuapp.com/stream/" + infoHash + "/" + (index + 1));
-				ws.send(JSON.stringify({
-					"type": "message",
-					"payload": "File converted!"
-				}));
-			}
-		} else {
-			ws.send(JSON.stringify({
-				"type": "error",
-				"payload": "Torrent is not ready!"
-			}));
-		}
-	} else {
-		ws.send(JSON.stringify({
-			"type": "error",
-			"payload": "Incorrect hash provided"
-		}));
-	}
 });
 router.get("/clear", function(req, res) {
-	Object.keys(clients).forEach(function(infoHash) {
-		clients[infoHash].destroy();
-		delete clients[infoHash];
-	});
+	client.destroy();
+	client = new WebTorrent();
 	fs.emptyDir("/tmp/webtorrent/");
 	res.send("<title>MiPeerFlix - Clear</title>Removed all!");
 });
-
 router.get("/download/:infoHash/:index?", async function(req, res) {
 	const infoHash = req.params.infoHash.toLowerCase();
 	if (infoHash.length == 40) {
-		if (!(infoHash in clients)) {
-			clients[infoHash] = new WebTorrent();
-		}
-		var torrent = clients[infoHash].get(infoHash);
+		var torrent = client.get(infoHash);
 		if (!torrent) {
-			torrent = clients[infoHash].add(buildMagnetURI(infoHash));
+			torrent = client.add(buildMagnetURI(infoHash));
 		}
 		if (torrent.ready) {
 			var index;
@@ -105,9 +48,8 @@ router.get("/download/:infoHash/:index?", async function(req, res) {
 				res.sendStatus(404);
 			}
 		} else {
-			setTimeout(function() {
-				res.redirect("/files/" + infoHash + (req.params.index ? "/" + req.params.index : ""));
-			}, 15000);
+			await sleep(15000);
+			res.redirect("/download/" + infoHash + (req.params.index ? "/" + req.params.index : ""));
 		}
 	} else {
 		res.sendStatus(404);
@@ -116,12 +58,9 @@ router.get("/download/:infoHash/:index?", async function(req, res) {
 router.get("/stream/:infoHash/:index?", async function(req, res) {
 	const infoHash = req.params.infoHash.toLowerCase();
 	if (infoHash.length == 40) {
-		if (!(infoHash in clients)) {
-			clients[infoHash] = new WebTorrent();
-		}
-		var torrent = clients[infoHash].get(infoHash);
+		var torrent = client.get(infoHash);
 		if (!torrent) {
-			torrent = clients[infoHash].add(buildMagnetURI(infoHash));
+			torrent = client.add(buildMagnetURI(infoHash));
 		}
 		if (torrent.ready) {
 			var index;
@@ -172,9 +111,8 @@ router.get("/stream/:infoHash/:index?", async function(req, res) {
 				res.sendStatus(404);
 			}
 		} else {
-			setTimeout(function() {
-				res.redirect("/files/" + infoHash + (req.params.index ? "/" + req.params.index : ""));
-			}, 15000);
+			await sleep(15000);
+			res.redirect("/stream/" + infoHash + (req.params.index ? "/" + req.params.index : ""));
 		}
 	} else {
 		res.sendStatus(404);
@@ -184,8 +122,8 @@ router.get("/list", function(req, res) {
 	res.set("cache-control", "no-store");
 	var html = "<html><head><title>MiPeerFlix - List</title><meta http-equiv=\"refresh\" content=\"20\"></head><body>";
 	if (Object.keys(clients).length) {
-		html += "<table>" + Object.values(clients).map(function(client) {
-			return "<tr><td>" + client.torrents[0].infoHash + "</td><td><a href =\"/" + client.torrents[0].infoHash + "\">" + (client.torrents[0].name ? client.torrents[0].name : client.torrents[0].infoHash) + "</a></td></tr>";
+		html += "<table>" + client.torrents.map(function(torrent) {
+			return "<tr><td>" + torrent.infoHash + "</td><td><a href =\"/" + torrent.infoHash + "\">" + (torrent.name ? torrent.name : torrent.infoHash) + "</a></td></tr>";
 		}).join("") + "</table>";
 	} else {
 		html += "No torrents in client!";
@@ -202,141 +140,6 @@ router.get("/:infoHash", function(req, res) {
 	}
 });
 exports.routes = router;
-
-function convertVideo(signal, input) {
-	return new Promise(async function(resolve, reject) {
-		const startTime = Date.now();
-		if (!await pingServers()) {
-			return;
-		}
-		const servers = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
-		const keyframes = await getKeyframes(signal, input);
-		let index = 1;
-		while (keyframes.length) {
-			let openServer = servers.indexOf(1);
-			if (openServer > -1) {
-				servers[openServer] = 0;
-				splitVideo(signal, openServer, input, crypto.randomBytes(20).toString("hex") + ".mp4", index, keyframes.shift(), keyframes[0]).then(function() {
-					servers[openServer] = 1;
-				});
-				index++;
-			} else {
-				console.log("Waiting for open server!");
-				await sleep(1000);
-			}
-		}
-		await Promise.resolve().then(function() {
-			resolve();
-		});
-		signal.addEventListener("abort", function() {
-			reject();
-		});
-		console.log("Processing time:", (Date.now() - startTime) / 1000);
-	});
-}
-
-function getKeyframes(signal, input) {
-	return new Promise(async function(resolve, reject) {
-		let keyframes = [];
-		await runCommand(signal, 0, "ffprobe -loglevel error -skip_frame nokey -select_streams v:0 -show_entries frame=pkt_pts_time -of csv=print_section=0 '" + input + "'", function(data) {
-			keyframes.push(data);
-		});
-		keyframes = Buffer.concat(keyframes).toString().split("\n").filter(function(el) {
-			return el.length;
-		});
-		await Promise.resolve().then(function() {
-			resolve(keyframes);
-		});
-		signal.addEventListener("abort", function() {
-			reject();
-		});
-	});
-}
-
-function pingServers() {
-	var promises = [];
-	for (var i = 0; i < 10; i++) {
-		promises.push(axios.get("http://w0rker" + i + ".herokuapp.com/ping").then(function(response) {
-			return response.data;
-		}));
-	}
-	return Promise.all(promises).then(function(data) {
-		return true;
-	}).catch(function() {
-		return false;
-	});
-}
-
-function runCommand(signal, server, command, stdout, stderr) {
-	return new Promise(function(resolve, reject) {
-		const ws = new WebSocket("wss://w0rker" + server + ".herokuapp.com/command");
-		ws.on("open", function() {
-			console.log("Connected");
-			if (!signal.aborted) {
-				ws.send(JSON.stringify({
-					"type": "message",
-					"payload": {
-						"command": command
-					}
-				}));
-			}
-		});
-		ws.on("message", function(string) {
-			let json = JSON.parse(string);
-			if (json.type == "stdout") {
-				if (stdout) {
-					stdout(Buffer.from(json.data));
-				} else {
-					console.log("stdout", Buffer.from(json.data).toString());
-				}
-			} else if (json.type == "stderr") {
-				if (stderr) {
-					stderr(Buffer.from(json.data));
-				} else {
-					console.log("stderr", Buffer.from(json.data).toString());
-				}
-			}
-		});
-		ws.on("error", function(err) {
-			console.log("Error", err);
-			reject();
-		});
-		ws.on("close", function() {
-			console.log("Disconnected");
-			if (signal.aborted) {
-				reject();
-			} else {
-				resolve();
-			}
-		});
-		signal.addEventListener("abort", function() {
-			ws.close();
-		});
-	});
-}
-
-function splitVideo(signal, server, input, output, index, start, end) {
-	return new Promise(async function(resolve, reject) {
-		let command = "ffmpeg -threads 4 -loglevel error -ss " + (parseFloat(start) - 0.1) + " -i " + input + " -ss " + parseFloat(start);
-		if (end) {
-			command += " -to " + parseFloat(end);
-		}
-		command += " -c:v libx264 -vf scale=-2:1080:flags=fast_bilinear -c:a copy -tune zerolatency -avoid_negative_ts 1 " + output;
-		await runCommand(signal, server, command);
-		const outputFile = fs.createWriteStream(__dirname.replace("/routes", "") + "/public/OUTPUT_" + index.toString().padStart(4, "0") + ".mp4");
-		await runCommand(signal, server, "cat " + output, function(data) {
-			outputFile.write(data);
-		});
-		outputFile.end();
-		await runCommand(signal, server, "rm " + output);
-		await Promise.resolve().then(function() {
-			resolve();
-		});
-		signal.addEventListener("abort", function() {
-			reject();
-		});
-	});
-}
 
 function updateStatus(ws, torrent) {
 	if (!ws) return;
@@ -367,12 +170,6 @@ function updateStatus(ws, torrent) {
 		if (status == "Downloaded") return;
 		return updateStatus(ws, torrent);
 	}, 1000);
-}
-
-function sleep(ms) {
-	return new Promise(function(resolve) {
-		setTimeout(resolve, ms);
-	});
 }
 
 function buildMagnetURI(infoHash) {
