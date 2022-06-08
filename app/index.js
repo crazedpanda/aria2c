3599da29637db3666c29efca3f143bfefb39da70
chouuohc87 <chouuohc87@yahoo.com>
2022-06-08 09:43:44 -0500

    Update index.js

--- app/index.js
+++ app/index.js
@@ -1,4 +1,4 @@
-const express = require("express");
+import express from "express";
 const app = express();
 const expressWs = require("express-ws")(app);
 const compression = require("compression");
